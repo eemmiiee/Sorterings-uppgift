982ef4cc45b277b87b7092d841284498f476f7e7
eemmiiee <92379069+eemmiiee@users.noreply.github.com>
2022-02-18 15:42:42 +0100

    Fungerande mergsort

--- Program.cs
+++ Program.cs
@@ -1,99 +1,98 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.Threading;
-namespace mergsort
+using System.Linq;
+using System.Text;
+
+namespace rilgheivb
+
 {
-   class MergeSort {
-  
-   
-    void merge(int[] arr, int l, int m, int r)
+    class Program
     {
-        
-        int n1 = m - l + 1;
-        int n2 = r - m;
-  
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-        int i, j;
-  
-      
-        for (i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
-  
-      
-        i = 0;
-        j = 0;
-  
-     
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();         
+
+            int antal = 8000;
+
+            //Merge Sort
+            List<int> unsorted = new List<int>();
+            List<int> sorted;
+
+            for(int c = 0; c < antal; c++){
+                unsorted.Add(rnd.Next());
             }
-            else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-  
-      
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-  
-        
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-    }
-  
-   
-    void sort(int[] arr, int l, int r)
-    {
-        if (l < r) {
+
+            Stopwatch mergeSW = new Stopwatch();
             
-            int m = l+ (r-l)/2;
-  
-           
-            sort(arr, l, m);
-            sort(arr, m + 1, r);
-  
-          
-            merge(arr, l, m, r);
+            mergeSW.Start();
+
+            sorted = MergeSort(unsorted);
+
+            mergeSW.Stop();
+
+            Console.WriteLine($"Merge Sort: {mergeSW.ElapsedMilliseconds}");
+
         }
+
+        private static List<int> MergeSort(List<int> unsorted)
+        {
+            if (unsorted.Count <= 1)
+                return unsorted;
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            int middle = unsorted.Count / 2;
+            for (int i = 0; i < middle;i++)
+            {
+                left.Add(unsorted[i]);
+            }
+            for (int i = middle; i < unsorted.Count; i++)
+            {
+                right.Add(unsorted[i]);
+            }
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+            return Merge(left, right);
+        }
+
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if(left.Count > 0 && right.Count > 0)
+                {
+                    if(left.First() <= right.First())
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());
+                    }
+                    
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }  
+                }
+
+                else if(left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+
+                else if(right.Count > 0)
+                {
+                    result.Add(right.First());
+                    right.Remove(right.First());
+                }
+            }
+            return result;
+        }
+
     }
-  
-    
-    static void printArray(int[] arr)
-    {
-        int n = arr.Length;
-        for (int i = 0; i < n; ++i)
-            Console.Write(arr[i] + " ");
-        Console.WriteLine();
-    }
-  
-   
-    public static void Main(String[] args)
-    {int []arr = new int[8000];
-    Random rnd = new Random();
-    for (int i = 0; i < 8000; i++) {
-  arr[i] = rnd.Next(0,1000) ;
-    }
-    Stopwatch stopwatch = new Stopwatch();
-    stopwatch.Start();
-     MergeSort ob = new MergeSort();
-        ob.sort(arr, 0, arr.Length - 1);
-        stopwatch.Stop();
-        Console.WriteLine("\nSorted array");
-        printArray(arr);
-        Console.WriteLine("Mergsort: {0} ", stopwatch.ElapsedMilliseconds);
 }
-   }}
