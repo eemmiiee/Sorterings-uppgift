5726679900c5c1811a3ef8961f0567067a300404
eemmiiee <89377694+naal01002@users.noreply.github.com>
2022-02-18 14:37:20 +0100

    e

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Emmie_Bubblesort___Insertionsort
+{
+
+    //https://csharpskolan.se/article/bubble-sort/
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            List<int> rndlist = new List<int>();
+            List<int> randList = new List<int>();
+
+            Stopwatch sw = new Stopwatch();
+            Stopwatch stopwatch = new Stopwatch();
+
+
+            for (int i = 0; i < 1000; i++){
+                rndlist.Add(rnd.Next());
+                randList.Add(rnd.Next());
+            }
+
+
+
+        //Bubblesort
+
+
+        }
+    }
+}
